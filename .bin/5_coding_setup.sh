d66b8ab4763cf8910c89cc6fa396324eb635eb67
Christian Riedel <cielquan@protonmail.com>
2021-05-17 18:31:45 +0200

    remove prompt from poetry

--- .bin/5_coding_setup.sh
+++ .bin/5_coding_setup.sh
@@ -30,7 +30,7 @@ done
 #   install and setup poetry
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - -y
 source $HOME/.poetry/env && poetry config virtualenvs.in-project true
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

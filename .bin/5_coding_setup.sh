65fe9b451e6a7ea7768b4de5e109300b299e4f43
Christian Riedel <cielquan@protonmail.com>
2021-05-17 18:30:14 +0200

    added missing sudo for vscode install

--- .bin/5_coding_setup.sh
+++ .bin/5_coding_setup.sh
@@ -52,8 +52,8 @@ sudo apt-get install -y nodejs 1> /dev/null
 if ! command -v code > /dev/null 2>&1; then
     printf "\n\n## Install VSCode\n"
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
     sudo apt-get update 1> /dev/null
     sudo apt-get install -y code 1> /dev/null

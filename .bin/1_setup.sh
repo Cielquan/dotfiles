6652561924da21065b200216717fe4c956f1f3fe
Christian Riedel <cielquan@protonmail.com>
2021-05-17 17:40:10 +0200

    improve messages

--- .bin/1_setup.sh
+++ .bin/1_setup.sh
@@ -33,6 +33,7 @@ clone_repo() {
 
 if answer_is_yes "Do you want to install the dotfiles?"; then
     clone_repo
+    printf "\n\n## Installer script's help page:\n"
     python3 ~/.dotfiles/.bin/2_install_dotfiles.py --help
     printf "\n\n## Please see the script's help page above. "
     printf "If you want to customize the install add your parameters before pressing enter.\n"

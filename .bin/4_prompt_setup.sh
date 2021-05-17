ed118b0bbd17fe4ffc8be82c9fe680300413eea2
Christian Riedel <cielquan@protonmail.com>
2021-05-17 18:19:46 +0200

    fix nerdfont download

--- .bin/4_prompt_setup.sh
+++ .bin/4_prompt_setup.sh
@@ -15,7 +15,8 @@ fi
 
 printf "\n\n## Installing nerdfont\n"
 current_version=$(curl -sSL -w %{url_effective} -o /dev/null https://github.com/ryanoasis/nerd-fonts/releases/latest | grep -Po 'v\d+\.\d+\.\d+')
-curl -sSL -o ~/Downloads/DejaVuSansMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/${current_version}/DejaVuSansMono.zip
+mkdir -p ~/Downloads
+wget -qO ~/Downloads/DejaVuSansMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/${current_version}/DejaVuSansMono.zip
 mkdir -p ~/.local/share/fonts
 unzip -o ~/Downloads/DejaVuSansMono.zip -d ~/.local/share/fonts/ 1> /dev/null
 fc-cache -f

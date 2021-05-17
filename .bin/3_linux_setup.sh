49f8e4ea4920bfb89ac99ca855bf48f870f0bd11
Christian Riedel <cielquan@protonmail.com>
2021-05-17 17:05:04 +0200

    put dist-upgrade after ppa adding

--- .bin/3_linux_setup.sh
+++ .bin/3_linux_setup.sh
@@ -17,14 +17,14 @@ sudo apt-get update
 printf "\n\n## Install certs and apt https\n"
 sudo apt-get install -y ca-certificates apt-transport-https
 
-printf "\n\n## Upgade all\n"
-sudo apt-get dist-upgrade -y
-
 printf "\n\n## Add git ppa\n"
 sudo add-apt-repository -y ppa:git-core/ppa
 
 printf "\n\n## Updating repos\n"
 sudo apt-get update
+
+printf "\n\n## Upgade all\n"
+sudo apt-get dist-upgrade -y
 
 echo ${PACKAGES} | tr ' ' '\n' | while read package; do
     printf "\n\n## Installing ${package}\n"

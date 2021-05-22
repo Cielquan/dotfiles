02ddf05ba8bd10d0ecbd2b7239f9405716b5ddc6
Christian Riedel <cielquan@protonmail.com>
2021-05-22 10:26:35 +0200

    make rust installer not prompt

--- bin/40_coding_setup.sh
+++ bin/40_coding_setup.sh
@@ -50,7 +50,7 @@ success "Done"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 info "Installing rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 success "Done"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

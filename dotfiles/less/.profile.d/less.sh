5e4af0b7db68b9f589829621ef3d4c779ebc55fb
Christian Riedel <cielquan@protonmail.com>
2021-05-25 11:20:28 +0200

    fix lesskey file format
    less needs the lesskey file to be in some binary format created by `lesskey`
    therefore renameing the file to `less` and updating the less.sh to compile
    lesskey file on boot if not existing or less file was updated

--- dotfiles/less/.profile.d/less.sh
+++ dotfiles/less/.profile.d/less.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
-LESSKEY=${HOME}/.config/less/lesskey
-export LESSKEY
+CONF_DIR=${HOME}/.config/less/
+LESSKEY=${CONF_DIR}/lesskey
+
+if ! [ -f "${LESSKEY}" ] || \
+    [ -n "$(find -L "${CONF_DIR}/less" -prune -newer "${LESSKEY}")" ]; then
+        lesskey -o "${LESSKEY}" "${CONF_DIR}/less"
+fi
+
+if [ -f "${LESSKEY}" ]; then
+    export LESSKEY
+fi

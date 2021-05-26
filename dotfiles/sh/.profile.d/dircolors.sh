7eeb82e8c0d12d902a30569a50d46acc9d4e052c
Christian Riedel <cielquan@protonmail.com>
2021-05-26 23:13:09 +0200

    fix if in dircolors

--- dotfiles/sh/.profile.d/dircolors.sh
+++ dotfiles/sh/.profile.d/dircolors.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
 # enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ] && [ -r ~/.dircolors ]; then
-    eval "$(dircolors -b ~/.dircolors)"
-else
-    eval "$(dircolors -b)"
+if [ -x /usr/bin/dircolors ]; then
+    if [ -r ~/.dircolors ]; then
+        eval "$(dircolors -b ~/.dircolors)"
+    else
+        eval "$(dircolors -b)"
+    fi
 fi

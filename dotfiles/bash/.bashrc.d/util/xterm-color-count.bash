c6890dbdf5b74558a34a9f91d869c63fa78552cb
Christian Riedel <cielquan@protonmail.com>
2026-05-04 14:08:35 +0200

    fix color count script to avoid leaking sequences to prompt

--- dotfiles/bash/.bashrc.d/util/xterm-color-count.bash
+++ dotfiles/bash/.bashrc.d/util/xterm-color-count.bash
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
-# Source: https://raw.githubusercontent.com/l0b0/xterm-color-count/master/xterm-color-count.sh
+# Original source: https://raw.githubusercontent.com/l0b0/xterm-color-count/master/xterm-color-count.sh
 
 trap 'tput sgr0' exit       # Clean up even if user hits ^C
 
+# Function to safely query terminal color support
+query_terminal_colors() {
+    local min=0
+    local max=256
 
-# First, test if terminal supports OSC 4 at all.
-printf '\e]4;%d;?\a' 0
-# shellcheck disable=2162
-read -d $'\a' -s -t 0.1 </dev/tty
-if [ -z "${REPLY}" ]; then
-    # OSC 4 not supported, so we'll fall back to terminfo 
-    max=$(tput colors)
-else
-    # OSC 4 is supported, so use it for a binary search 
-    min=0
-    max=256
-    while [[ $((min+1)) -lt $max ]]; do
-        i=$(( (min+max)/2 ))
-        printf '\e]4;%d;?\a' $i
-        # shellcheck disable=2162
-        read -d $'\a' -s -t 0.1 </dev/tty
-        if [ -z "${REPLY}" ]; then
-            max=${i}
-        else
-            min=${i}
-        fi
-    done
-fi
+    # First, test if terminal supports OSC 4 at all
+    printf '\e]4;%d;?\a' 0
 
+    # Read with longer timeout and proper cleanup
+    # shellcheck disable=2162
+    if read -d $'\a' -s -t 0.5 REPLY 2>/dev/null; then
+        # OSC 4 is supported, use binary search
+        while [[ $((min+1)) -lt ${max} ]]; do
+            local i=$(( (min+max)/2 ))
+            printf '\e]4;%d;?\a' "${i}"
 
-SUPPORTED_COLORS=${max}
-unset max
+            # shellcheck disable=2162
+            if read -d $'\a' -s -t 0.5 REPLY 2>/dev/null && [[ -n "${REPLY}" ]]; then
+                min=${i}
+            else
+                max=${i}
+            fi
+        done
+    else
+        # OSC 4 not supported, fall back to terminfo
+        max=$(tput colors 2>/dev/null || echo 8)
+    fi
+
+    echo "${max}"
+}
+
+SUPPORTED_COLORS=$(query_terminal_colors)
 export SUPPORTED_COLORS
+
+# Ensure clean output
+tput sgr0

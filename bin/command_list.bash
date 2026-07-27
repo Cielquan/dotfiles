8ebae8dc35dba0202c51340b3922257d56ba4b89
Christian Riedel <cielquan@protonmail.com>
2026-07-27 15:18:08 +0200

    add command list

---
+++ bin/command_list.bash
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2317
+
+echo "This file is not intended to be run!"
+exit 1
+
+
+# -----------------------------------------------------------------------------
+# Basics (APT)
+# -----------------------------------------------------------------------------
+sudo apt install ca-certificates apt-transport-https
+sudo apt install nano curl unzip htop ldnsutils net-tools wget
+
+
+# -----------------------------------------------------------------------------
+# Needed for SMB network share
+# -----------------------------------------------------------------------------
+sudo apt install cifs-utils
+
+
+# -----------------------------------------------------------------------------
+# Docker setup
+# -----------------------------------------------------------------------------
+sudo apt install docker.io docker-compose
+# Add current user to docker group to reduce need for sudo with docker
+sudo usermod -aG docker "${USER}"
+
+
+# -----------------------------------------------------------------------------
+# `brave-browser`
+# -----------------------------------------------------------------------------
+sudo curl -fsSL --proto '=https' --tlsv1.2 -o /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSL --proto '=https' --tlsv1.2 -o /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+
+
+# -----------------------------------------------------------------------------
+# Additional PPAs
+# -----------------------------------------------------------------------------
+sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:phoerious/keepassxc # NOTE: Flatpak is recommended for `keepassxc`
+
+
+# -----------------------------------------------------------------------------
+# LS_COLORS
+# -----------------------------------------------------------------------------
+echo "check LS_COLORS for updates: https://github.com/trapd00r/LS_COLORS/"
+
+
+# -----------------------------------------------------------------------------
+# Python (additional versions)
+# -----------------------------------------------------------------------------
+sudo add-apt-repository ppa:deadsnakes/ppa
+export V=3.14 && sudo apt install "python${V}" "python${V}-dev"
+
+
+# -----------------------------------------------------------------------------
+# Rust
+# -----------------------------------------------------------------------------
+curl -fsSL --proto '=https' --tlsv1.2 https://sh.rustup.rs | sh
+
+
+# -----------------------------------------------------------------------------
+# PNPM
+# -----------------------------------------------------------------------------
+curl -fsSL --proto '=https' --tlsv1.2 https://get.pnpm.io/install.sh | sh -
+
+
+# -----------------------------------------------------------------------------
+# Ruby (Needed by some pre-commit hooks)
+# -----------------------------------------------------------------------------
+sudo apt install ruby-full
+
+
+# -----------------------------------------------------------------------------
+# Nerdfont (needed by starship)
+# -----------------------------------------------------------------------------
+echo "Install a nerdfont"
+
+
+# -----------------------------------------------------------------------------
+# Starship prompt
+# -----------------------------------------------------------------------------
+cargo install --locked starship
+sudo apt install starship
+curl -sS https://starship.rs/install.sh | sh
+
+
+# -----------------------------------------------------------------------------
+# uv (Python toolchain)
+# -----------------------------------------------------------------------------
+cargo install --locked uv
+curl -fsSL --proto '=https' --tlsv1.2 https://astral.sh/uv/install.sh | sh
+
+
+# -----------------------------------------------------------------------------
+# VSCodium (`codium`)
+# -----------------------------------------------------------------------------
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.sources
+echo "ext list in dotfiles/vscodium/.config/VSCodium/User/extensions.txt"
+
+# -----------------------------------------------------------------------------
+# Godot
+# -----------------------------------------------------------------------------
+echo "Download godot from in ~/bin: https://godotengine.org/download/linux/"
+# `godot` wrapper is in `dotfiles`; `godot4` is needed by a VSCode ext
+ln -s ~/bin/godot ~/bin/godot4
+# `godot-raw` is used in above wrapper script
+ln -s ~/bin/godot-raw ~/bin/...

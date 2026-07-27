5a391ccebfcd641a506522148aa380c8111940e6
Christian Riedel <cielquan@protonmail.com>
2026-07-27 21:25:37 +0200

    fix linting issues

--- dotfiles/pdbpp/.pdbrc.py
+++ dotfiles/pdbpp/.pdbrc.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
+"""Custom PDB++ config.
+
+Last commit on PDB++ was 2022-07-14, so it seems to be dead.
+"""
+
 import pdb
 
 
-class Config(pdb.DefaultConfig):
+class Config(pdb.DefaultConfig):  # ty:ignore[unresolved-attribute]
+    """Custom PDB config."""
 
     editor = "nano"
     stdin_paste = "epaste"
-    filename_color = pdb.Color.lightgray
+    filename_color = pdb.Color.lightgray  # ty:ignore[unresolved-attribute]
     use_terminal256formatter = False
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Init custom PDB++ config."""
         try:
-            from pygments.formatters import terminal
+            from pygments.formatters import terminal  # noqa: PLC0415
         except ImportError:
             pass
         else:
@@ -25,8 +33,9 @@ class Config(pdb.DefaultConfig):
                 }
             )
 
-    def setup(self, pdb):
+    def setup(self, pdb) -> None:  # noqa: ANN001
+        """Set up PDB++."""
         # make 'l' an alias to 'longlist'
-        Pdb = pdb.__class__
+        Pdb = pdb.__class__  # noqa: N806
         Pdb.do_l = Pdb.do_longlist
         Pdb.do_st = Pdb.do_sticky
